c02c43d512f52fe7afa29cbdb51481afad0c7986
Oleg Bannyi <oleg.bannyi@gmail.com>
2024-11-04 00:49:49 +0200

    Create top-duration-for-each-company.sql

---
+++ top-duration-for-each-company.sql
@@ -0,0 +1,19 @@
+with RouteDuration as (
+select
+	ac.company_name,
+	t.town_from AS departure_city,
+	t.town_to AS arrival_city,
+    avg(timestampdiff(minute, t.time_out, t.time_in)) as avg_flight_duration
+	from Airline_company ac
+	join Trip t
+	using(ID_comp)
+    group by company_name, departure_city, arrival_city
+	),
+RouteAvgDuration as (
+    select *,
+    row_number() over(partition by company_name order by avg_flight_duration desc) as row_num
+    from RouteDuration
+)
+select company_name, departure_city, arrival_city, avg_flight_duration
+from RouteAvgDuration
+where row_num < 3;
